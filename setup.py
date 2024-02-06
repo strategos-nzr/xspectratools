d420cac40cc5ff147fdd448a7e8b4bf946a47619
strategos-nzr <nzr111@protonmail.com>
2024-02-06 10:48:32 -0500

    removed unmaintained packages

--- setup.py
+++ setup.py
@@ -5,31 +5,26 @@ Created on Fri Jul  8 15:12:59 2022
 @author: Nick
 """
 
-from setuptools import setup,find_packages
+from setuptools import setup
 
 setup(name='xspectratools',
-      version = '0.0.8',
-      description='Read and Process ALS Beamline Data',
+      version = '0.0.9',
+      description='Read and Process RIXS data',
       author = "Nick Russo",
       author_email="nzr111@protonmail.com",
       url = '',
-      packages = ['xspectratools','xspectratools.diffraction'],
+      packages = ['xspectratools'],
       py_modules=[
       'io',
       'xas',
-      'rixs',
-      'arpes',
-      'uncertainty',
-      'sampling',
-      'diffraction'
+      'rixs'
 
       ],
-      package_dir={"xspectratools":"xspectratools",
-                    "xspectratools.diffraction":"xspectratools/diffraction"},
+      package_dir={"xspectratools":"xspectratools"},
       install_requires=[
         'pandas',
         'numpy',
         'matplotlib',
-        'adaptive','scipy'
+        'scipy'
     ]
       )
